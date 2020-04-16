8cb0f11f273479f1d232f4277eaf4d5b26051b2d
Nikita Niverovskii <heket844@gmail.com>
2020-04-16 15:28:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
+app.use(express.static(__dirname, 'dist', {index: false}));
+
 app.get('/*', function(req,res) {
  
 res.sendFile(path.join(__dirname+'/src/index.html'));
