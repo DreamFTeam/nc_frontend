1ada8caf727785f3cadd621bc77473fb650ea3a3
Dan <lyash.dv@gmail.com>
2020-05-17 01:05:33 +0300

    add deactivate and delete quiz

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import * as jwt_decode from 'jwt-decode';
-import { User } from '../_models/user';
 import {AuthenticationService} from '../_services/authentication.service';
 
 @Injectable({
@@ -10,11 +8,11 @@ import {AuthenticationService} from '../_services/authentication.service';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       const info = this.authenticationService.currentUserValue;
 
       if (next.data.roles && next.data.roles.indexOf(info.role) === -1) {
