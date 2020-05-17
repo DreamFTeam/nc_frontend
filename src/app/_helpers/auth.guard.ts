4c333d803ed455d2f74c9f5b32b3ce6474173bc9
Dan <lyash.dv@gmail.com>
2020-05-17 12:51:38 +0300

    fix user view errors

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import {AuthenticationService} from '../_services/authentication.service';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const info = this.authenticationService.currentUserValue;
+    const info = this.authenticationService.currentUserValue;
 
+    if (info.role) {
       if (next.data.roles && next.data.roles.indexOf(info.role) === -1) {
         return false;
+      }
     }
-
-      return true;
+    return true;
 
   }
 
