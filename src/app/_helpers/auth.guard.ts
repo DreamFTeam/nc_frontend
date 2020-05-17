d01779c67212d102e58570b6d0bc28168221fdd7
Ihor Paprotskyi <igorpapr@gmail.com>
2020-05-17 12:53:36 +0300

    small fixes of validation page layout

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import * as jwt_decode from 'jwt-decode';
-import { User } from '../_models/user';
-import {AuthenticationService} from '../_services/authentication.service';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const info = this.authenticationService.currentUserValue;
+    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const info = this.authenticationService.currentUserValue;
 
+    if (info.role) {
       if (next.data.roles && next.data.roles.indexOf(info.role) === -1) {
         return false;
+      }
     }
-
-      return true;
+    return true;
 
   }
 
--- src/app/_models/quiz-validation-preview.ts
+++ src/app/_models/quiz-validation-preview.ts
@@ -16,7 +16,6 @@ export class QuizValidationPreview implements DesearizableWImage {
 
   deserialize(input: any, sanitizer: DomSanitizer): this {
     Object.assign(this, input);
-    console.log(input);
     const img = this.imageContent;
     if (img !== null) { //don't add url data inside if it is null - then use mock img in html
       const objUrl = 'data:image/jpeg;base64,' + img;
