14565a27cb16c7c7c7b2a685591591fd6c2445f3
MistleHC <ertdfgcvnwarr@gmail.com>
2020-05-18 17:54:21 +0300

    User change password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { SuggestionsComponent } from './suggestions/suggestions.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { YesNoModalComponent } from './yes-no-modal/yes-no-modal.component';
 import { UserInvitationsComponent } from './user-invitations/user-invitations.component';
+import { UserChangePasswordComponent } from './user-change-password/user-change-password.component';
 
 const appRoutes: Routes = [];
 
@@ -84,7 +85,8 @@ const appRoutes: Routes = [];
     SuggestionsComponent,
     ActivitiesComponent,
     YesNoModalComponent,
-    UserInvitationsComponent],
+    UserInvitationsComponent,
+    UserChangePasswordComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
