e46e6e93d1a6c54bd9cb8ec194683b1e2e7d4e34
Dan <lyash.dv@gmail.com>
2020-05-26 22:10:12 +0300

    add links for activities

--- src/app/modules/core/_models/activity.ts
+++ src/app/modules/core/_models/activity.ts
@@ -8,11 +8,31 @@ export class Activity implements DesearizableWImage {
     datetime: Date;
     imageContent: any;
     title: string;
+    link: string;
 
 
     deserialize(input: any, sanitizer: DomSanitizer): this {
         Object.assign(this, input);
         this.id = input.activityId;
+
+        switch(input.activityTypeId) { 
+            case 1: { 
+               this.link = "/profile/"+input.linkInfo;
+               break; 
+            } 
+            case 2: { 
+                this.link = "/profile/"+input.linkInfo+"/3";
+               break; 
+            } 
+            case 3: { 
+               this.link = "/game/result/"+input.linkInfo;
+               break; 
+            } 
+            case 4: { 
+                this.link = "/viewquiz/"+input.linkInfo;
+                break; 
+             } 
+         }
 
         if (this.imageContent !== null) {
             const objUrl = 'data:image/jpeg;base64,' + this.imageContent;
