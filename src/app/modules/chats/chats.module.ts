cf7140d4e4320ab68f766a8710c3f06da270e3c0
Never04eK <heket844@gmail.com>
2020-05-27 22:28:56 +0300

    Add chat settings

--- src/app/modules/chats/chats.module.ts
+++ src/app/modules/chats/chats.module.ts
@@ -5,11 +5,12 @@ import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
 import { ChatComponent } from './chat/chat.component';
 import { CreateChatComponent } from './create-chat/create-chat.component';
+import { ChatSettingsComponent } from './chat-settings/chat-settings.component';
 
 
 
 @NgModule({
-  declarations: [ChatsPageComponent, ChatComponent, CreateChatComponent],
+  declarations: [ChatsPageComponent, ChatComponent, CreateChatComponent, ChatSettingsComponent],
   imports: [
     CommonModule,
     SharedModule,
